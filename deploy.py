08c0ebff3de3e801bf4ad7fa8e4c801e1005cc09
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2018-02-28 12:21:53 +0000

    Add metadata to deploy script

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import argparse
 import os
 import yaml
@@ -29,6 +30,7 @@ for field in mandatory_fields:
             raise Exception("Mandatory field '"+field+"' not specified in config file")
 
 # build commandline
+now = datetime.datetime.now()
 image_name = config['name'] + '-' + config['version']
 image_file = 'target/' + image_name + '.qcow2'
 ram = config['deploy']['min_ram']
@@ -36,14 +38,25 @@ hdd = config['deploy']['min_hd']
 maintainers = config['maintainers']
 
 cli = 'openstack image create --disk-format qcow2 --file ' + image_file
+cli += ' --property version="' + config['version'] + '"'
+cli += ' --property name="' + config['name'] + '"'
+cli += ' --property upload_date="' + now.isoformat() + '"'
 if ram:
     cli += ' --min-ram ' + str(ram)
 if hdd:
     cli += ' --min-disk ' + str(hdd)
 if maintainers:
     cli += ' --property maintainers="' + str.join(',',maintainers) + '"'
+if 'OS_USERNAME' in os.environ:
+    uploader = os.environ['OS_USERNAME']
+    cli += ' --property uploader="' + uploader + '"'
+if 'description' in config:
+    cli += ' --property description="'+ config['description'] +'"'
+if 'tags' in config:
+    for tag in config['tags']:
+        cli += ' --tag "' + tag + '"'
 
-cli += ' ' + image_name
+cli += ' "' + image_name + ' (' + now.strftime("%Y-%m-%d") + ')"'
 
 sys.stderr.write("Executing: " + cli + "\n")
 sys.exit(os.system(cli))
